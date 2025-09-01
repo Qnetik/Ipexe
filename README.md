e3c77a9075f2ff7d0415eac3424e7976d7fe59ce
AstraExe <vextris@mail.ru>
2025-09-01 09:11:02 +0300

    v2

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
-
-## Short description:
- -This program is designed for quick basic IP address checking.
- -It uses the ipwho.is API and the IP address validation method via Python's ipaddress module.
+## IpExe:  
+This tool is designed for quick basic IP and website checking.
+It uses the [ipwho.is](https://ipwhois.io/) API, and the IP/URL address validation method via Python's `ipaddress` and `urlparse` module.
 
 
 ## Installation:
-  ## Option 1
-  -pip install git+https://github.com/Qnetik/ip-checker.git
-  
-  ## Option 2
-  -git clone https://github.com/Qnetik/ip-checker.git
-  -cd ip-checker
-  -pip install -r requirements.txt  # install dependencies
-  -pip install .                    # install the package
 
+### Option 1 (via pip)
+```bash
+pip install git+https://github.com/Qnetik/ip-checker.git
+```
+### Option 2 (via clone)
+
+```bash
+git clone https://github.com/Qnetik/ip-checker.git
+cd ip-checker
+pip install -r requirements.txt  
+pip install .                    
+```
 
 ## Usage:
- -ipexe ip
+```bash
+ipexe <IP_ADDRESS_or_URL>
+```
 
  
   
--- ipexe/ip_checker.py
+++ ipexe/ip_checker.py
@@ -1,52 +1,68 @@
 import requests 
 import time
 import sys
+import socket
 import ipaddress
 from rich.console import Console 
+from urllib.parse import urlparse
 console = Console()
-
 
 def error(msg):
     console.print(f"Error: {msg}", style="bold red")
     time.sleep(1)
 
-def mainstart(ip_user):
-    try: 
-        ipaddress.ip_address(ip_user)
+def mark(value):
+    return f"[bold green][+][/]" if value not in (None, "Nope") else f"[bold red][-][/]"
+
+def mainstart(target: str):
+    try:
+       ipaddress.ip_address(target)
+       final_ip = target
     except ValueError: 
-        error("Invalid IP address format")
-        return
-    
-    url = f"http://ipwho.is/{ip_user}"
+        try:
+           url = target.strip().lower()
+           parsed = urlparse(url)
+           domain = parsed.netloc if parsed.netloc else parsed.path
+           domain = domain.rstrip("/")
+           final_ip = socket.gethostbyname(domain)
+        except socket.gaierror:
+           error("Invalid IP or domain")
+           return None
+ 
+    url = f"https://ipwho.is/{final_ip}"
 
     try:
-        response = requests.get(url, timeout=5)
-        if response.status_code != 200:
-            error("Code 200 !")
-            return
-        dataget = response.json()  
+       response = requests.get(url, timeout=5)
+       if response.status_code != 200:
+           error("Code 200 !")
+           return
+       dataget = response.json()  
     except Exception as e:
-        error(e)
-        return
+       error(str(e))
+       return
     
-    if not dataget.get("success", False):
-        error("API request unsuccessful")
-        return
-    
-    datainfo = f"""
-    Success: {dataget.get('success', 'Nope')}  
-    Type: {dataget.get('type', 'Nope')} 
-    Continent: {dataget.get('continent', 'Nope')} 
-    Country: {dataget.get('country', 'Nope')} 
-    Region: {dataget.get('region', 'Nope')} 
-    City: {dataget.get('city', 'Nope')}  
-    Postal: {dataget.get('postal', 'Nope')} 
-    coordinates: {dataget.get('latitude', 'Nope')}/{dataget.get('longitude', 'Nope')}
-    Provider: {dataget.get('connection', {}).get('org', 'Nope')}
-    Timezone: {dataget.get('timezone', {}).get('abbr', 'Nope')}
-    Time: {dataget.get('timezone', {}).get('current_time', 'Nope')}"""
 
-    print(datainfo)
+    if not dataget.get("success", False):
+       error("API request unsuccessful")
+       return
+    
+    
+
+
+    datainfo = f"""{mark(dataget.get('success', 'Nope'))} Success: {dataget.get('success', 'Nope')}
+{mark(dataget.get('type', 'Nope'))} Type: {dataget.get('type', 'Nope')}
+{mark(dataget.get('ip', 'Nope'))} Ip: {dataget.get('ip', 'Nope')}
+{mark(dataget.get('continent', 'Nope'))} Continent: {dataget.get('continent', 'Nope')}
+{mark(dataget.get('country', 'Nope'))} Country: {dataget.get('country', 'Nope')}
+{mark(dataget.get('region', 'Nope'))} Region: {dataget.get('region', 'Nope')}
+{mark(dataget.get('city', 'Nope'))} City: {dataget.get('city', 'Nope')}
+{mark(dataget.get('postal', 'Nope'))} Postal: {dataget.get('postal', 'Nope')}
+{mark(dataget.get('latitude', 'Nope'))} Coordinates: {dataget.get('latitude', 'Nope')}/{dataget.get('longitude', 'Nope')}
+{mark(dataget.get('connection', {}).get('org', 'Nope'))} Provider: {dataget.get('connection', {}).get('org', 'Nope')}
+{mark(dataget.get('timezone', {}).get('abbr', 'Nope'))} Timezone: {dataget.get('timezone', {}).get('abbr', 'Nope')}
+{mark(dataget.get('timezone', {}).get('current_time', 'Nope'))} Time: {dataget.get('timezone', {}).get('current_time', 'Nope')}"""
+
+    console.print(datainfo, highlight=False)
 
 def main():
     if len(sys.argv) != 2:
